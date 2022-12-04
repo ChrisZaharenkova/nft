e62f401851fd2dc4c8f3d92e3c9167f6e73337c4
ChrisZaharenkova <kristina.zaharenkova00@mail.ru>
2022-12-04 17:33:25 +0300

    slider upgrade

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-$(document).ready(function(){
-  let 
-})
-
+let slide = $('.saller__card'),
+    slider = $('.saller__cards'),
+    countSlide=slide.length,
+    position=0,
+    shag = 200,
+    widthWindow = $('.slider').width(),
+    max = (countSlide * 200) - widthWindow;
+$('.next').click(function(){
+  console.log('WindowWidth:'+widthWindow)
+  console.log('Maximum:'+max)
+  if(position == -max){
+    position = 0;
+    slider.css('left',position+'px');
+  }else{
+    position=position-shag;
+    slider.css('left',position + 'px');
+  }
+});
+$('.prev').click(function(){
+  if(position == 0){
+    position = -max;
+    slider.css('left',position+'px');
+  }else{
+    position = position + shag;
+    slider.css('left',position+'px');
+  }
+});
 
 
 //плавный скролл
